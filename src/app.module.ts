d2eba54cf76719bb5b04abc2f8a88e698645f8f8
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2025-09-19 14:34:31 -0300

    feat: Adiciona alguns providers e módulos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import SupabaseProvider from '@shared/providers/supabase.provider';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
+  providers: [SupabaseProvider]
 })
-export class AppModule {}
+export class AppModule { }
