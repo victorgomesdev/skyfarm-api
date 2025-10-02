cd809f5e53b8c904d45ce7a9fb677254fd8b33cf
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2025-10-02 17:24:27 -0300

    fix: Corrije o funcionamento do AuthGuard.

--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -1,30 +1,41 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { createLocalJWKSet, jwtVerify } from "jose";
-import * as set from "../../../jwtset.json";
+import { jwtVerify, createRemoteJWKSet } from "jose";
 import { Request } from "express";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private config: ConfigService) {}
+  constructor(private config: ConfigService) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest<Request>();
-    const token = request.headers["authorization"]?.split(" ")[1];
 
-    if (!token) throw new UnauthorizedException('Token não fornecido');
+    const request = context.switchToHttp().getRequest<Request>()
+    const token = request.headers["authorization"]?.split(" ")[1]
+
+    if (!token) throw new UnauthorizedException("Token não fornecido")
 
     try {
-      const key = createLocalJWKSet(set as any);
 
-      const { payload } = await jwtVerify(token, key, {
-        issuer: `${this.config.get('SB_URL')}/auth/v1`,
-      });
+      const key = this.config.get('JWT_KEY_URL')
+      const k = createRemoteJWKSet(new URL(key))
 
-      if(payload.exp as number * 1000 > Date.now())  throw new UnauthorizedException()
-      return true;
+      const { payload } = await jwtVerify(token, k, {
+        issuer: `${this.config.get("SB_URL")}/auth/v1`,
+      })
+
+      if ((payload.exp as number) * 1000 < Date.now()) {
+        throw new UnauthorizedException("Token expirado")
+      }
+
+      request.headers = {
+        ...request.headers,
+        "authorization": payload.sub
+      }
+
+      return true
     } catch (err) {
-      throw new UnauthorizedException('Token inválido ou expirado');
+      throw new UnauthorizedException("Token inválido ou expirado")
     }
   }
+
 }
